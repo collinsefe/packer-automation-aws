1779bb6b0d89ff3624e257c13329ea15c6ce385f
Collins Orighose <collinsefe@yahoo.com>
2025-03-04 22:33:07 +0000

    refactor code

--- packer-linux/user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-# sudo echo "<h1>Welcome My Good People, Welcome! This server is running on $(hostname -I)</h1>" > /var/www/html/index.html
-
